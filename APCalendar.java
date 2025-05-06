75cbb9b78a7379218939800c5ba14cf5d116ad71
Ashley <167535539+hello135790@users.noreply.github.com>
2025-05-06 22:25:59 +0000

    Final with test cases

--- APCalendar.java
+++ APCalendar.java
@@ -2,14 +2,25 @@ public class APCalendar
 {
     /** Returns true if year is a leap year and false otherwise. */
     private static boolean isLeapYear(int year)
-    { /* implementation not shown */ }
+    {
+        boolean four = year % 4 == 0;
+        boolean hundred = year % 100 == 0;
+        boolean fourhundred = year % 400 == 0;
+        return four && (!hundred || fourhundred);
+    }
    
     /** Returns the number of leap years
     * between year1 and year2, inclusive.
     * Precondition: 0 <= year1 <= year2
     */
     public static int numberOfLeapYears(int year1, int year2)
-    { /* to be implemented in part (a) */ }
+    {
+        int count = 0;
+        for (int i = year1; i<=year2; i++) {
+            if (isLeapYear(i)) count++;
+        }
+        return count;
+    }
    
     /** Returns the value representing the day
     * of the week for the first day of year,
@@ -17,7 +28,14 @@ public class APCalendar
     * ..., and 6 denotes Saturday.
     */
     private static int firstDayOfYear(int year)
-    { /* implementation not shown */ }
+    {
+        int day = 1;
+        for(int i=1; i<year; i++) {
+            day++;
+            if (isLeapYear(i)) day++;
+        }
+        return day % 7;
+    }
    
     /** Returns n, where month, day, and year specify
     * the nth day of the year. Returns 1 for January 1
@@ -26,7 +44,23 @@ public class APCalendar
     * year is a valid date.
     */
     private static int dayOfYear(int month, int day, int year)
-    { /* implementation not shown */ }
+    {
+        int days = 0;
+        if (month>1) days += 31;
+        if (month>2) days += 28;
+        if (isLeapYear(year)) days++;
+        if (month>3) days += 31;
+        if (month>4) days += 30;
+        if (month>5) days += 31;
+        if (month>6) days += 30;
+        if (month>7) days += 31;
+        if (month>8) days += 31;
+        if (month>9) days += 30;
+        if (month>10) days += 31;
+        if (month>11) days += 30;
+        days += day;
+        return days;
+    }
    
     /** Returns the value representing the day of the week for
     * the given date (month, day, year), where 0 denotes Sunday,
@@ -35,7 +69,10 @@ public class APCalendar
     * year is a valid date.
     */
     public static int dayOfWeek(int month, int day, int year)
-    { /* to be implemented in part (b) */ }
+    {
+        int days = dayOfYear(month, day, year) % 7;
+        return firstDayOfYear(year) + days - 1;
+    }
    
     // There may be instance variables, constructors,
     // and other methods not shown.
